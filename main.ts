5e313e9e2fef34523579b1d660de1f6fa242e865
gulten35 <78615029+gulten35@users.noreply.github.com>
2021-02-05 21:28:11 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(3)
+    basic.pause(100)
+    basic.showNumber(2)
+    basic.pause(100)
+    basic.showNumber(1)
+    basic.pause(100)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.StickFigure)
+    basic.pause(100)
+    basic.showIcon(IconNames.Giraffe)
+    basic.pause(100)
+})
